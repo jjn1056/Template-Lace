830894b692e05c9704b236b3fed4fdeeece5d53c
John Napiorkowski <jjn1056@yahoo.com>
2017-04-19 11:04:06 -0500

    more docs

--- lib/Template/Lace.pm
+++ lib/Template/Lace.pm
@@ -12,9 +12,74 @@ Template::Lace - Logic-less and strongly typed templates, with components
 
     TBD
 
+=head1 DISCLAIMER
+
+L<Template::Lace> is a toolkit for building HTML pages using logic-less and componentized
+templates.  As such this distribution is currently not aimed at standalone use but rather
+exists as all the reusable bits that fell out when I refactored L<Catalyst::View::Template::Lace>.
+Currently this toolkit then exists to support the L<Catalyst> View and as a result documentation
+here is high level and API level.  If you want to integrate L<Template::Lace> into other
+web frameworks you might wish to review L<Catalyst::View::Template::Lace> for a possible
+approach.
+
 =head1 DESCRIPTION
 
-    TBD
+L<Template::Lace> is a toolkit that makes it possible to bind HTML templates to plain old Perl
+classes (although we will canonically use L<Moo> in these examples, L<Moo> is not a requirement
+just a suggestion).  These templates are fully HTML markup only; they contain no display logic,
+only valid HTML and component declarations.  We use L<Template::Lace::DOM> (which is a subclass
+of L<Mojo::DOM58>) to alter the template for presentation at request time.  L<Template::Lace::DOM>
+provides an API to transform the template.  We call this a Model class.  For a Perl class to be
+considered Model class it should provide the interface defined by L<Template::Lace::ModelRole>.
+Generally if you are using L<Moo> or L<Moose> you should consume this role (athough strictly
+speaking that isn't necessary as long as your class provides the minimal interface.)  Here's
+an example of a very simple Model class:
+
+    package  MyApp::View::User;
+
+    use Moo;
+    with 'Template::Lace::ModelRole';
+
+    has [qw/age name motto/] => (is=>'ro', required=>1);
+
+    sub process_dom {
+      my ($self, $dom) = @_;
+      $dom->dl('#user', +{
+        age=>$self->age,
+        name=>$self->name,
+        motto=>$self->motto});
+    }
+
+    sub template { q[
+      <html>
+        <head>
+          <title>User Info</title>
+        </head>
+          <body>
+            <dl id='user'>
+              <dt>Name</dt>
+              <dd id='name'> -NAME- </dd>
+              <dt>Age</dt>
+              <dd id='age'> -AGE- </dd>
+              <dt>Motto</dt>
+              <dd id='motto'> -MOTTO- </dd>
+            </dl>
+          </body>
+      </html>
+    ]}
+
+    1;
+
+In this example the Model class defines two methods, C<process_dom> and C<template>.  Any Perl class
+can be used as Model class as long as it provides these methods (as well as a stub for C<prepare_dom>
+which will we discuss later; or just consume L<Template::Lace::ModelRole>.)  The C<template> method
+just needs to return a string.  It should contain your desired markup.  The C<process_dom> method
+is an instance method on your class, and it gets both C<$self> and C<$dom>, where C<$dom> is a
+DOM representation of your template (via L<Template::Lace::DOM>).  Anything you want to change about
+the template should be done via the L<Template::Lace::DOM> API.  This is a subclass of L<Mojo::DOM58>
+a Jquery like API for transforming HTML.  Our custom subclass contains some additional helper methods
+to make common types of transforms easier.  For example here we use the custom helper C<dl> to find
+a C<dl> tag by its id and then populate its data by matching hash keys to tag ids.
 
 =head1 IMPORTANT NOTE REGARDING VALID HTML
 
